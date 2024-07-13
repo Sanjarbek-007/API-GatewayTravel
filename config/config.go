9c1a7082b3e53b1daa9084ee89dbcd7f977db64b
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-07-14 02:02:58 +0500

    Api

---
+++ config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/cast"
+)
+
+type Config struct {
+	HTTPPort string
+
+	PostgresHost     string
+	PostgresPort     int
+	PostgresUser     string
+	PostgresPassword string
+	PostgresDatabase string
+
+	DefaultOffset string
+	DefaultLimit  string
+
+	TokenKey string
+}
+
+func Load() Config {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("No .env file found")
+	}
+
+	config := Config{}
+
+	config.HTTPPort = cast.ToString(getOrReturnDefaultValue("HTTP_PORT", ":8089"))
+
+	config.PostgresHost = cast.ToString(getOrReturnDefaultValue("POSTGRES_HOST", "localhost"))
+	config.PostgresPort = cast.ToInt(getOrReturnDefaultValue("POSTGRES_PORT", 5432))
+	config.PostgresUser = cast.ToString(getOrReturnDefaultValue("POSTGRES_USER", "macbookpro"))
+	config.PostgresPassword = cast.ToString(getOrReturnDefaultValue("POSTGRES_PASSWORD", "1111"))
+	config.PostgresDatabase = cast.ToString(getOrReturnDefaultValue("POSTGRES_DATABASE", "users"))
+	config.TokenKey = cast.ToString(getOrReturnDefaultValue("TokenKey", "my_secret_key"))
+	return config
+}
+
+func getOrReturnDefaultValue(key string, defaultValue interface{}) interface{} {
+	val, exists := os.LookupEnv(key)
+
+	if exists {
+		return val
+	}
+
+	return defaultValue
+}
